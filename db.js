9814344610cfddf78aec7a9fe9b3bd105dfbf0f0
MichalStokluska <mstokluska@localhost.localdomain>
2019-08-22 13:06:06 +0100

    feat: initial rest server

---
+++ db.js
@@ -0,0 +1,54 @@
+
+const users = [
+  {
+    id: '1',
+    firstName: 'Michael',
+    lastName: 'Stone',
+    title: 'Mr',
+    email: 'michael@example.com',
+    taskId: '20',
+  },
+  {
+    id: '2',
+    firstName: 'John',
+    lastName: 'Barry',
+    title: 'Mr',
+    email: 'john@example.com',
+    taskId: '21',
+  },
+  {
+    id: '3',
+    firstName: 'Mary',
+    lastName: 'Savage',
+    title: 'Mrs',
+    email: 'mary@example.com',
+    taskId: '22',
+  },
+];
+
+const tasks = [
+  {
+    id: '20',
+    title: 'Restocking',
+    description: 'please restock soft drinks section',
+    status: 'to be completed',
+    assignedTo: '1',
+  },
+  {
+    id: '21',
+    title: 'Cleaning',
+    description: 'please clean your desk!',
+    status: 'to be completed',
+    assignedTo: '2',
+  },
+  {
+    id: '22',
+    title: 'Documentation update',
+    description: 'please update our customers details',
+    status: 'to be completed',
+    assignedTo: '3',
+  },
+];
+
+module.exports.users = users;
+module.exports.tasks = tasks;
---
+++ readme.md
@@ -0,0 +1,5 @@
+NOTE: THIS IS PART 3/4 FOR MY TUTORIAL THAT CAN BE FOUND ON DEV.TO
+
+----
+#<center>GraphQL</center>
+----
